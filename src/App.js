1d70e83b8869b6114a2aa6cb6f3ad96fcf5714eb
BlazingIsFire <firemanandy1102@gmail.com>
2022-12-26 14:53:34 -0500

    Modified Routes.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Routes } from 'react-router-dom';
 import { AuthProvider } from "./contexts/AuthContext";
 import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
+import Home from './pages/Home';
 import PrivateRoute from './components/PrivateRoute';
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" element={<Login />}/>
         {/* PrivateRoutes */}
           <Route element={<PrivateRoute />}>
-            <Route path='home' element={<Dashboard/>}/>
+            <Route path='home' element={<Home/>}/>
           </Route>
       </Routes>
     </AuthProvider>
