1e1166a275adff7d5bf364dadb813d881a8b2d9e
BlazingIsFire <firemanandy1102@gmail.com>
2022-12-26 17:43:50 -0500

    Removed settings button and replaced it with a dark mode switch.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,27 +2,39 @@ import React, { useState, useEffect } from 'react';
 import '../index.css';
 import './Home.css';
 import Note from '../components/Note';
-import { ReactComponent as Settings } from '../imgs/setting.svg';
 import { ReactComponent as Add } from '../imgs/add.svg';
 
 
 function Home() {
     const [username, setUsername] = useState('Blazing');
+    const [settingsModal, setSettingsModal] = useState(false);
   return (
     <>
     <div className='home-container'>
         <div className='home-header flex'>
             <h1 className='font-carter-one'>Noteboard</h1>
-            <span className='font-carter-one flex pointer'>Settings
-                <Settings id='settings-svg'/>
-            </span>
+            <div className='home-header-dark-container'>
+                <label className='font-carter-one'>Dark Mode</label>
+               <span className='home-header-dark-mode'>
+                    <input type='checkbox' />
+                    <span className='dark-mode-slider pointer'/>
+                </span> 
+            </div>
+            
         </div>
         <div className='home-body flex-center-all'>
-            <div className='home-new-note flex-center-all flex-column'>
+            <div className='home-new-note flex-center-all flex-column pointer'>
                 <Add className='pointer' id='new-note'/>
                 <h2 className='font-carter-one'>Add Note</h2>
             </div>
             <Note />
+            <Note />
+        </div>
+    </div>
+    <div className={`${settingsModal ? 'settings-modal-container flex-center-all' : 'display-none'}`}>
+        <div className='settings-modal-box flex-center-all'>
+            <h1>Noteboard Settings:</h1>
+                        
         </div>
     </div>
     </>
