cf4784d2155b5326195342198dc5890b08e9c927
BlazingIsFire <firemanandy1102@gmail.com>
2022-12-30 19:13:40 -0500

    Added dynamic data for each note. Each note now displays its color and data that it was created with.

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import '../index.css';
 import './Note.css'
+import { db } from '../firebase';
+import { useAuth } from '../contexts/AuthContext';
+import { deleteDoc, doc } from 'firebase/firestore';
 import { ReactComponent as Trashbin } from '../imgs/trash.svg';
 
-function Note() {
+function Note({ note, theme }) {
+  // sets theme to dark or light mode
+  const pageTheme = theme;
+  // Auth
+  const { currentUser } = useAuth();
+  // all useStates
+  const [noteDate, setNoteDate] = useState(null);
+
+  useEffect(()=>{
+    findDate()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  // finds the date of the note, else sets it at todays date.
+  const findDate = async () =>{
+    try{
+      const firebaseNoteDate = await note.noteDate;
+      const noteDateOrg = new Date(firebaseNoteDate.seconds * 1000).toLocaleDateString('en-US');
+      setNoteDate(noteDateOrg)
+    }catch{
+      const today = new Date()
+      const todayDate = today.toLocaleDateString('en-US');
+      setNoteDate(todayDate);
+    }
+  }
+
+  // Deletes an note
+  const handleNoteDelete = async () => {
+    await deleteDoc(doc(db, `noteboard-app/${currentUser.uid}/userNotes`, note.noteId))
+  }
+
   return (
     <>
-    <div className='note pointer'>
-        <div className='note-header flex-center-all' style={{backgroundColor: '#ff0000'}}>
-            <h4 className='font-carter-one'>01/01/0000</h4>
-            <Trashbin className='pointer' id='trash-svg'/>
+    <div className='note pointer' id={pageTheme}>
+        <div className='note-header flex' style={{backgroundColor: `${note.noteColor}`}}>
+                <h4 className='font-carter-one'>{noteDate}</h4>
+            <Trashbin className='pointer' id='trash-svg' onClick={handleNoteDelete}/>
         </div>
         <div className='note-body flex flex-column'>
-            <h1 className='note-title'>Test Title</h1>
-            <p className='note-content'>This is some text.</p>
+            <h1 className='note-title'>{note.noteTitle}</h1>
+            <p className='note-content'>{note.noteContent}</p>
         </div>
-    </div>  
+    </div>
     </>   
   )
 }
